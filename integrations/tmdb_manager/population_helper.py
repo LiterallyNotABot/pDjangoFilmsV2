9efc0c8b9ca993029f0e71db21b25511ddea9500
Matias <matpadron26@gmail.com>
2025-04-26 20:02:48 +0200

    cambio nombre

--- integrations/tmdb_manager/population_helper.py
+++ integrations/tmdb_manager/population_helper.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-from integrations.tmdb_integration.language_helper import iso_639_1_to_fullname
-from integrations.tmdb_integration.tmdb_connector import get_person_details
+from integrations.tmdb_manager.language_helper import iso_639_1_to_fullname
+from integrations.tmdb_manager.tmdb_connector import get_person_details
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
