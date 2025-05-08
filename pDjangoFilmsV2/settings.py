eec4ea77ca9e55612a8921e94333718d45057146
Matias <matpadron26@gmail.com>
2025-05-08 14:18:43 +0200

    act

--- pDjangoFilmsV2/settings.py
+++ pDjangoFilmsV2/settings.py
@@ -177,6 +177,9 @@ CHANNEL_LAYERS = {
     },
 }
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 # KEYS
 
 STRIPE_SECRET_KEY = "sk_test_51QuZCQPdMh0chZHFYp8nRrBRKEYncGoaNefqfKSqKprIdP6pKYa26SqW5g8zqivZwQKrBZdK4NsYL09VzOxrp7ND00u6cY2GL1"
